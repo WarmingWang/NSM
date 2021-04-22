be270ba6000aca3db75243f04ac430476393d7ae
WarmingWang <wangmin.warming@gmail.com>
2021-04-22 16:07:56 +0800

    xxx

---
+++ NSM/src/taobao/taobao.py
@@ -0,0 +1,114 @@
+import time
+import json
+import os
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from NSM.src.utils import utils
+
+
+class TaoBao:
+    fileCWD = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        self.get_user_info()
+        if utils.is_windows():
+            options.binary_location = self.chromepath
+
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("prefs", {"prfile.managed_default_content_setting.images": 2})
+        self.browser = webdriver.Chrome(options=options, executable_path=self.get_chromedriver_exe_path())
+
+        # 反爬设置  webdriver为 undefined
+        self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """
+        			Object.defineProperty(navigator, 'webdriver', {
+        			  get: () => undefined
+        			})
+        		  """
+        })
+        self.wait = WebDriverWait(self.browser, 10)
+        self.loginurl = "https://login.taobao.com/member/login.jhtml"
+        self.trytime = 0
+        self.excelfile = "%s.xlsx" % self.keyword
+
+    # 从config.json 文件中读取相关的配置
+    def get_user_info(self):
+        with open("config.json", "r", encoding="utf-8") as f:
+            config = json.load(f)
+
+        self.username = config["username"]
+        self.password = config["password"]
+        self.keyword = config["keyword"]
+        self.maxpage = config["maxpage"]
+        self.chromepath = config["chromepath"]
+        # print(self.password)
+
+    # 获取不同平台的chromedriver的路径
+    def get_chromedriver_exe_path(self):
+        ret = "./bin/mac/chromedriver"
+        if utils.is_windows():
+            ret = "D:\Python\Python39\Scripts\chromedriver.exe"
+        return ret
+
+    def login(self):
+        self.browser.get(self.loginurl)
+        try:
+            # 找到用户名输入框,输入账号密码并登录
+            username_input = self.wait.until(EC.presence_of_element_located((By.ID, "fm-login-id")))
+            username_input.send_keys(self.username)
+
+            password_input = self.wait.until(EC.presence_of_element_located((By.ID, "fm-login-password")))
+            password_input.send_keys(self.password)
+
+            login_button = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "fm-button")))
+            login_button.click()
+            time.sleep(5)
+            # # 检查是否需要验证码登陆
+            # while 1:
+            #     try:
+            #         self.browser.find_element_by_id('J_Phone_Checkcode')
+            #     except:
+            #         break
+            # time.sleep(2)
+            # ”site-nav-login-info-nick” 找到名字标签并打印内容
+            taobao_name_tag = self.wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, "site-nav-login-info-nick ")))
+            print(f"登录成功:{taobao_name_tag.text}")
+        except Exception as e:
+            print(e)
+            self.browser.close()
+            print("登录失败")
+
+    # 爬取相关的内容
+    def crawl(self):
+        self.detail_page('600344949083')
+
+    # 爬详情页面信息
+    def detail_page(self, productId):
+        print('detail_page start...')
+        url = "https://detail.tmall.com/item.htm?id=" + productId
+        self.browser.get(url)
+        time.sleep(2)
+        # 滚动条操作
+        new_height = self.browser.execute_script("return document.body.scrollHeight;")
+        for i in range(math.ceil(new_height/500)):
+            self.browser.execute_script('window.scrollTo(0, '+str(500*(i+1))+')')
+            time.sleep(1)
+
+        items = self.browser.find_elements_by_xpath('//*[@id="description"]/div/p/img')
+        for idx, item in enumerate(items):
+            pic = item.get_attribute("src")
+            utils.saveImg(pic, self.fileCWD + '\\pic\\' + productId, str(idx) + '.jpg')
+            print(pic)
+
+
+if __name__ == "__main__":
+    tb = TaoBao()
+    tb.login()
+    tb.crawl()
---
+++ NSM/src/utils/utils.py
@@ -0,0 +1,130 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import gzip
+import platform
+import time
+import json
+import random
+import logging
+import datetime
+import requests
+import urllib.request
+from urllib.request import urlopen, Request
+
+logger = logging.getLogger(__name__)  # 操作日志对象
+logging.basicConfig(level=logging.ERROR)
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+}
+
+def get_urlopen(url_home, ret_type):
+    i = 0
+    while 1:
+        try:
+            # html = getHtml(url_home, headers=headers)
+            html = urlopen(Request(url_home, headers=headers)).read()
+            break
+        except:
+            i += 1
+            if i >= 5:
+                logger.error('访问失败%d次，请检查！', i)
+                return []
+            logger.debug('访问失败%d次，1-5秒后尝试再次连接', i)
+            time.sleep(random.randint(1, 5))
+            continue
+    if ret_type == 'json':
+        try:
+            return json.loads(ungzip(html).decode('utf-8'))
+        except:
+            return []
+    elif ret_type == 'pic':
+        return html
+    else:
+        return ungzip(html)
+"""
+解压
+"""
+
+
+def ungzip(data):
+    try:  # 尝试解压
+        # print('正在解压.....')
+        data = gzip.decompress(data)
+        # print('解压完毕!')
+    except:
+        pass
+        # print('未经压缩, 无需解压')
+    return data
+
+
+"""
+YYYYMMDD -> YYYY-MM-DD
+"""
+
+
+def strToDate(date: str):
+    if len(date) != 8:
+        logger.exception("错误的日期字符串！%s", date)
+    try:
+        int(date)
+    except Exception as e:
+        logger.exception("错误的日期字符串！%s", date)
+        logger.exception(e)
+    return date[0:4] + '-' + date[4:6] + '-' + date[6:8]
+
+
+"""
+获取当前日期YYYY-MM-DD
+"""
+
+
+def getCurDate() -> str:
+    return datetime.datetime.today().strftime('%Y-%m-%d')
+
+
+def quotedstr(S) -> str:
+    return '\'' + S + '\''
+
+
+def get_proxy():
+    return requests.get("http://127.0.0.1:5010/get/").json()
+
+
+def delete_proxy(proxy):
+    requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
+
+
+def getHtml(url, headers):
+    retry_count = 5
+    while retry_count > 0:
+        try:
+            proxy = get_proxy().get("proxy")
+            httpproxy_handler = urllib.request.ProxyHandler({"http": "http://{}".format(proxy)})
+            opener = urllib.request.build_opener(httpproxy_handler)
+            request = urllib.request.Request(url, headers=headers)
+            response = opener.open(request)
+            html = response.read()
+            return html
+        except Exception:
+            retry_count -= 1
+    # 删除代理池中代理
+    delete_proxy(proxy)
+    return None
+
+def saveImg(imageURL, absDir, fileName):
+    data = get_urlopen(imageURL, 'pic')
+    if not os.path.exists(absDir):
+        os.makedirs(absDir)
+    f = open(absDir+'\\'+fileName, 'wb')
+    f.write(data)
+    f.close()
+
+
+# 判断是否是windows系统
+def is_windows():
+    return platform.system().lower() == "windows"
